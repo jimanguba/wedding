91d92cca6a0882cd8e7a4f63b3e63bdd87f5de79
jimanguba <manguba3@myumanitoba.ca>
2025-04-14 00:36:23 -0500

    fix: deployment error

--- components/RegistrySection.js
+++ components/RegistrySection.js
@@ -14,7 +14,7 @@ export default async function RegistrySection() {
         <>
           <p className="text-gray-600 text-lg leading-relaxed">
             Your love and presence mean the world to us, and we truly
-            aren't expecting anything more. But if you’d like to bless us
+            aren't expecting anything more. But if you&apos;d like to bless us
             as we start this new chapter together,
             <span className="block mt-2 font-medium text-[color:#800000] dark:text-[#BE7F89]">
               we’ve put together a little registry with love 💝
@@ -39,7 +39,7 @@ export default async function RegistrySection() {
       ) : (
         <>
           <p className="text-gray-600 dark:text-white text-lg leading-relaxed">
-            Your presence is more than enough, and we're not expecting any
+            Your presence is more than enough, and we&apos;re not expecting any
             gifts. But, if you wish to contribute to our married life,
             <span className="block mt-2 font-medium text-[color:#800000] dark:text-[#BE7F89]">
               come back soon to see our registry
