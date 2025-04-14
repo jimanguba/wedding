f009cdaae552f65e1c4ea502a4ca32838a27b863
jimanguba <manguba3@myumanitoba.ca>
2025-04-14 00:45:50 -0500

    FIX #2: replaced ' with apos

--- components/RegistrySection.js
+++ components/RegistrySection.js
@@ -14,7 +14,7 @@ export default async function RegistrySection() {
         <>
           <p className="text-gray-600 text-lg leading-relaxed">
             Your love and presence mean the world to us, and we truly
-            aren't expecting anything more. But if you&apos;d like to bless us
+            aren&apos;t expecting anything more. But if you&apos;d like to bless us
             as we start this new chapter together,
             <span className="block mt-2 font-medium text-[color:#800000] dark:text-[#BE7F89]">
               we’ve put together a little registry with love 💝
